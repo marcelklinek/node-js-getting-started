ab7bd6cd9149a18a1c5760b0e949fd68900355bf
marcelklinek <marcel.klinek@ibillboard.com>
2019-11-29 16:41:29 +0100

    switch the port for 5000

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { HttpServer, requestHandler } from './http-server';
 
 function startServer() {
 	const host = "127.0.0.1"
-	const port = process.env.PORT || 9001
+	const port = process.env.PORT || 500
 
 	new HttpServer(requestHandler, host, port).init();
 }	
