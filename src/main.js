73e9b785cc74680f0c54b69fcb60efd246c285f6
gianpiero-bozza <gianpiero.bozza@ibillboard.com>
2019-12-03 09:41:48 +0100

    changed main to port 3000

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { HttpServer, requestHandler } from './http-server';
 
 function startServer() {
-	const port = process.env.PORT || 9001;
+	const port = process.env.PORT || 3000;
 
 	new HttpServer(requestHandler, port).init();
 }	
-// call http://localhost:9001/getAd
+
 startServer()
