fdf4164070fc951b7d31e0e3e779a45eef892808
gianpiero-bozza <gianpiero.bozza@ibillboard.com>
2019-12-03 08:24:44 +0100

    fixed tag.js and slightly modified example page

--- src/tag.js
+++ src/tag.js
@@ -1,9 +1,15 @@
 export const tag = `
-<pre>
-	<code contenteditable spellcheck="false">
+<code><pre>
+&lt;script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous">
+&lt;/script&gt;
+&lt;script&gt;
+    $(document).ready(function() {
         $.get("https://shrouded-headland-00949.herokuapp.com/getAd?publisherId=YOUR_ID", function(data) {
             $("#YOUR_CONTAINER_ID").html(data);
         });
-    </code>
-</pre>
+    });
+&lt;/script&gt;
+</pre></code>
 `;
