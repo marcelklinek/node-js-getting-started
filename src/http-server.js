db41c9f64fafa2c535927a147fba19c6a8046df6
gianpiero-bozza <gianpiero.bozza@ibillboard.com>
2019-12-04 14:39:21 +0100

    refactoring code and tests after reharsal

--- src/http-server.js
+++ src/http-server.js
@@ -17,32 +17,33 @@ export class HttpServer {
             return http.createServer(this.handlerFn).listen(this.port);
         }
     }
-}
 
-export function requestHandler(req, res) {
-    const parsedUrl = url.parse(req.url,true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    if (req.method == 'GET') {
-        const publisherId = parsedUrl.query.publisherId;
-        const containsPublisherId = publisherId ? true : false;
-
-        if (parsedUrl.pathname == '/getAd') { 
-            if (containsPublisherId) {
-                const msg = `Publisher with id ${publisherId} requested an ad`;
+    static requestHandler(req, res) {
+        const parsedUrl = url.parse(req.url, true);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    
+        if (req.method == 'GET') {
+            const publisherId = parsedUrl.query.publisherId;
+            const containsPublisherId = publisherId ? true : false;
+    
+            if (parsedUrl.pathname == '/getAd') { 
+                if (containsPublisherId) {
+                    const msg = `Publisher with id ${publisherId} requested an ad`;
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(msg);
+                } else {
+                    res.writeHead(422);
+                }
+            } else if (parsedUrl.pathname == '/getTag') {
                 res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(msg);
+                res.write(tag);
             } else {
-                res.writeHead(422);
+                res.writeHead(404);
             }
-        } else if (parsedUrl.pathname == '/getTag') {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(tag);
         } else {
-            res.writeHead(404);
+            res.writeHead(405);
         }
-    } else {
-        res.writeHead(405);
+        
+        res.end();
     }
-    res.end();
 }
